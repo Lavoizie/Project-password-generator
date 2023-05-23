a39f26fad2f2edecdef660f7ec681461a3692289
Lavoizie Carvalho Guimaraes <72814732+Lavoizie@users.noreply.github.com>
2023-05-23 12:07:17 -0300

    commit valores aleatorios
    Adiciona o código JavaScript que gera letras (Maiúsculas e Minúsculas), números e caracteres especiais para gerar a senha posteriormente.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
 //seleção de elementos
  const generatedPasswordButton = document.querySelector("#generate-password");
- const generatedPasswordElement = document.querySelector("#generate-password");
+ const generatedPasswordElement = document.querySelector("#generated-password");
 
 //funções
+//Letras, Números e Símbolos
+const getLetterLowerCase = () =>{
+   return String.fromCharCode( Math.floor(Math.random() *26) +97 );
+};
+
+const getLetterUpperCase = () =>{
+    return String.fromCharCode( Math.floor(Math.random() *26) +65 );
+ };
+
+ const getNumber = () => {
+    return Math.floor(Math.random() *10).toString();
+ }
+
+ const getSynbol = () => {
+    const symbols = "()-+=&*%¨#$!@{}[]?/<>"
+    return symbols[Math.floor(Math.random()* symbols.length)];
+ }
+
+console.log(getSynbol());
 
 //eventos
-generatedPasswordButton.addEventListener('click', () => {
-    console.log("teste");
-});
